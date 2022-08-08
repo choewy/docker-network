27c2b6b09b94435c024e7835b9f95febb97bd31e
choewy <choewy@choewyui-MacBookPro.local>
2022-08-08 10:38:33 +0900

    [refector] code change using funtion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
-import { typeOrmModule } from './utils/tools/typeorm';
+import { configModule } from './utils/modules/config';
+import { typeOrmModule } from './utils/modules/typeorm';
 
 @Module({
-  imports: [typeOrmModule(), UsersModule],
+  imports: [configModule(), typeOrmModule(), UsersModule],
   controllers: [AppController],
   providers: [Logger],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,35 @@
+import helmet from 'helmet';
+import * as express from 'express';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { winstonLogger } from './utils/tools/winston';
-import { loggerMiddleware } from './utils/middlewares/logger.middleware';
+import { winstonLogger } from './utils/modules/winston';
+import { loggerMiddleware } from './utils/middlewares/logger';
+import { ConfigService } from '@nestjs/config';
+import { ServerConfig } from './utils/config/server.config';
+import { swaggerModule } from './utils/modules/swagger';
 
 const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule, {
-    logger: winstonLogger(),
-  });
+  const logger = winstonLogger();
+  const app = await NestFactory.create(AppModule, { logger });
+  const config = app.get(ConfigService).get<ServerConfig>('server');
 
+  app.use(helmet());
+  app.use(express.json({ limit: config.limit }));
+  app.use(
+    express.urlencoded({
+      limit: config.limit,
+      extended: true,
+    }),
+  );
+
+  app.enableCors(config.cors);
   app.use(loggerMiddleware(new Logger()));
-
-  /* 
-    (global)UserGuard
-    => AuthGuard 
-    => RolesGuard 
-  */
   app.useGlobalGuards();
 
-  await app.listen(3001, '0.0.0.0');
+  swaggerModule(app);
+
+  await app.listen(config.port, config.host);
 };
 
 bootstrap();
