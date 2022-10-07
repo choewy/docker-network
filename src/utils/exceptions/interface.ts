ccd7a225650a40fd7d8785d4444fd6bfd203d581
choewy <choewy32@gmail.com>
2022-10-07 09:58:42 +0900

    reset: project

--- src/utils/exceptions/interface.ts
+++
@@ -1,9 +0,0 @@
-import { ApiResponseProperty } from '@nestjs/swagger';
-
-export class HttpErrorData {
-  @ApiResponseProperty()
-  statusCode: number;
-
-  @ApiResponseProperty()
-  message: string;
-}
--- src/utils/middlewares/logger.ts
+++
@@ -1,9 +0,0 @@
-import { Logger } from '@nestjs/common';
-import { RequestHandler } from '@nestjs/common/interfaces';
-
-export const loggerMiddleware = (logger: Logger): RequestHandler => {
-  return (req, res, next) => {
-    logger.log('LoggerMiddleware');
-    next();
-  };
-};
