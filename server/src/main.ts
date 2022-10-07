a9bb5633b085f61507dd8bcd903d423b3a9ca9db
choewy <choewy32@gmail.com>
2022-10-07 10:15:45 +0900

    feature: server - docker-compose

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const PORT = 3001;
+const HOST = '::';
+
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+
+  app.use(
+    json({
+      limit: '2GB',
+    }),
+  );
+
+  app.use(
+    urlencoded({
+      limit: '2GB',
+      extended: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
+  await app.listen(PORT, HOST);
+};
+
 bootstrap();
